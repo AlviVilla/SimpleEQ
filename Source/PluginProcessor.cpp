1108b7b8c82f827acac9db2096e7c2949dcd35cd
AlviVilla <a.villa6@hotmail.com>
2025-04-12 18:30:22 +0200

    added save state

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -8,6 +8,8 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "juce_core/juce_core.h"
+#include "juce_data_structures/juce_data_structures.h"
 #include "juce_dsp/juce_dsp.h"
 
 //==============================================================================
@@ -176,6 +178,7 @@ bool SimpleEQAudioProcessor::hasEditor() const
 juce::AudioProcessorEditor* SimpleEQAudioProcessor::createEditor()
 {
     // return new SimpleEQAudioProcessorEditor (*this);
+    // Here I add a generic GUI to test the funct. By default it would have used the hello world example
     return new juce::GenericAudioProcessorEditor(*this);
 }
 
@@ -185,12 +188,20 @@ void SimpleEQAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);
 }
 
 void SimpleEQAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if(tree.isValid()){
+        apvts.replaceState(tree);
+        updateFilters();
+    }
 }
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts){
